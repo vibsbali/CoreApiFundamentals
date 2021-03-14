cb9eab44ae01e4a1ef63ec41e08a39ed74c03a14
Vaibhav Bali <vbali@internode.on.net>
2021-03-14 20:53:00 +1100

    add versioning add multiple versioning method

--- src/Startup.cs
+++ src/Startup.cs
@@ -8,6 +8,7 @@ using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +31,19 @@ namespace CoreCodeCamp
             {
                 opt.AssumeDefaultVersionWhenUnspecified = true;
                 opt.ReportApiVersions = true;
+                //single version reader
+                //opt.ApiVersionReader = new HeaderApiVersionReader("X-Version");
+
+                //specifying multiple version readers
+                opt.ApiVersionReader = ApiVersionReader.Combine(
+                    new HeaderApiVersionReader("X-Version"),
+                    new QueryStringApiVersionReader("ver", "version"));
+
+                //specify version in url
+                //opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+                //the following is what is added on all controller that requires version 
+                //[Route("api/v{version:apiVersion}/[Controller]")]
+                //http://localhost:6600/api/v2/camps
             });
 
             services.AddControllers();
