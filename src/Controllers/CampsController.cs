9983b63c954363cce96d222f45a82276318c27ba
Vaibhav Bali <vbali@hub24.com.au>
2021-03-19 13:31:52 +1000

    finished lecture 4 modifying data

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -8,19 +8,23 @@ using AutoMapper;
 using CoreCodeCamp.Data;
 using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 
 namespace CoreCodeCamp.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class CampsController : ControllerBase
     {
         private readonly ICampRepository _campRepository;
         private IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
 
-        public CampsController(ICampRepository campRepository, IMapper mapper)
+        public CampsController(ICampRepository campRepository, IMapper mapper, LinkGenerator linkGenerator)
         {
             _campRepository = campRepository;
             _mapper = mapper;
+            _linkGenerator = linkGenerator;
         }
 
         [HttpGet]
@@ -67,10 +71,91 @@ namespace CoreCodeCamp.Controllers
 
                 return Ok(_mapper.Map<CampModel[]>(results));
             }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CampModel>> Post(CampModel campModel)
+        {
+            try
+            {
+                var existingCamp = await _campRepository.GetCampAsync(campModel.Moniker);
+                if (existingCamp != null)
+                {
+                    return BadRequest("Moniker is in use");
+                }
+                
+                var location = _linkGenerator.GetPathByAction("Get", "Camps", new {moniker = campModel.Moniker});
+                if (string.IsNullOrWhiteSpace(location))
+                {
+                    return BadRequest("Could not use current moniker");
+                }
+
+                var camp = _mapper.Map<Camp>(campModel);
+                _campRepository.Add(camp);
+                if (await _campRepository.SaveChangesAsync())
+                {
+                    return Created($"/api/camps/{camp.Moniker}", _mapper.Map<CampModel>(camp));
+                }
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{moniker}")]
+        public async Task<ActionResult<CampModel>> Put(string moniker, CampModel campModel)
+        {
+            try
+            {
+                var existingCamp = await _campRepository.GetCampAsync(campModel.Moniker);
+                if (existingCamp == null)
+                {
+                    return NotFound($"Could not find camp with moniker of {moniker}");
+                }
+
+                _mapper.Map(campModel, existingCamp);
+                if (await _campRepository.SaveChangesAsync())
+                {
+                    return _mapper.Map<CampModel>(existingCamp);
+                }
+            }
             catch (Exception e)
             {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
             }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{moniker}")]
+        public async Task<IActionResult> Delete(string moniker)
+        {
+            try
+            {
+                var existingCamp = await _campRepository.GetCampAsync(moniker);
+                if (existingCamp == null)
+                {
+                    return NotFound($"Could not find camp with moniker of {moniker}");
+                }
+                
+                if (await _campRepository.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            return BadRequest();
         }
     }
 }
