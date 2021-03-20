ab0b2013c5278e163d98797393b33aecda664026
Vaibhav Bali <vbali@internode.on.net>
2021-03-20 19:52:30 +1100

    added operations controller

---
+++ src/Controllers/OperationsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreCodeCamp.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public OperationsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = _configuration as IConfigurationRoot;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
